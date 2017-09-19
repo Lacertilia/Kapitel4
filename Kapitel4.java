aedff8bef4c6e2d95aa9d165332e329e81b490cb
Rasmus Mattas <mara16@nti.umea>
2017-09-19 21:14:59 +0200

    4.2

--- Kapitel4.java
+++ Kapitel4.java
@@ -9,7 +9,7 @@ public class Kapitel4{
 		char a2 = '\u00E4'; //ä
 		char o1 = '\u00F6'; //ö
 
-		String min = JOptionPane.showInputDialog(null, "Ungef" + a2 + "r hur m" + a1 + "nga minuter ringer du per m" + a1 + "nad?");
+		/*String min = JOptionPane.showInputDialog(null, "Ungef" + a2 + "r hur m" + a1 + "nga minuter ringer du per m" + a1 + "nad?");
 		String costS = JOptionPane.showInputDialog(null, "Hur mycket kostar det f" + o1 + "r dig att ringa minutvis?");
 		double minutes = Double.parseDouble(min);
 		double costD = Double.parseDouble(costS);
@@ -19,6 +19,18 @@ public class Kapitel4{
 		} else {
 			product = product * 0.9;
 			JOptionPane.showMessageDialog(null, "Det kostar ungef" + a2 + "r " + product + " kr per m" + a1 + "nad.");
+		}*/
+		String OTicket = JOptionPane.showInputDialog(null, "Hur mycket kostar det att gymma en g" + a1 + "ng?");
+		int OTW = Integer.parseInt(OTicket);
+		String YTicket = JOptionPane.showInputDialog(null, "Hur mycker kostar ett " + a1 + "rskort?");
+		int YTW = Integer.parseInt(YTicket);
+		String ggr = JOptionPane.showInputDialog(null, "Hur m" + a1 + "nga g" + a1 + "nger kommer du gymma?");
+		int gr = Integer.parseInt(ggr);
+		OTW = OTW * gr;
+		if(OTW >= YTW){
+			JOptionPane.showMessageDialog(null, "Det l" + o1 + "nar sig att ta ett " + a1 + "rskort!");
+		}else {
+			JOptionPane.showMessageDialog(null, "Det l" + o1 + "nar sig inte att ta ett " + a1 + "rskort!");
 		}
 	}
 }
